4ea9495f8335e41eff6b85ff0d93be88a8a43f04
JordiNodeJS <jordi.nodejs@gmail.com>
2021-11-15 00:31:14 +0100

    Juego Adivina

---
+++ js/adivinaDoom.js
@@ -0,0 +1,6 @@
+const dom = {
+  ranges: document.querySelectorAll('input[type="range"]'),
+  badges: document.querySelectorAll('.badge'),
+}
+
+export default dom
---
+++ js/main.js
@@ -0,0 +1,7 @@
+import dom from './adivinaDoom.js'
+
+for (let i = 0; i < dom.ranges.length; i++) {
+  dom.ranges[i].addEventListener('input', () => {
+    dom.badges[i].textContent = dom.ranges[i].value
+  })
+}

b74c9a7e32c524ca1f89f3f132d915e956235102
JordiNodeJS <jordi.nodejs@gmail.com>
2021-11-15 01:03:37 +0100

    Incluyo modal

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 import dom from './adivinaDoom.js'
 
-for (let i = 0; i < dom.ranges.length; i++) {
+for (let i = 0; i < dom.ranges.length; i++)
   dom.ranges[i].addEventListener('input', () => {
     dom.badges[i].textContent = dom.ranges[i].value
   })
-}

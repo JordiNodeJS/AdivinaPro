3fdbeb1a1f7f789172146e8e127e8e7082939fd1
JordiNodeJS <jordi.nodejs@gmail.com>
2021-11-15 23:33:21 +0100

    desde casa v 2.1

--- js/main.js
+++ js/main.js
@@ -3,49 +3,45 @@ import dom, {
   checkRandom,
   counter,
   rememberGuessNumbers,
-} from './adivinaDoom.js';
+} from './adivinaDoom.js'
 
 for (let i = 0; i < dom.ranges.length; i++)
   dom.ranges[i].addEventListener('input', () => {
-    dom.badges[i].textContent = dom.ranges[i].value;
-  });
+    dom.badges[i].textContent = dom.ranges[i].value
+  })
 
 for (let i = 0; i < dom.bodyModalranges.length; i++) {
-  dom.bodyModalranges[i].textContent = dom.ranges[i].value;
+  dom.bodyModalranges[i].textContent = dom.ranges[i].value
 }
 
-let random;
-let count = 1;
-console.log(++count);
+let random
+let count = 1
+console.log(++count)
 
 adivina.addEventListener('click', () => {
-  random = getRandom(dom.ranges);
-  console.log(random);
-});
+  random = getRandom(dom.ranges)
+  alerts.textContent = `Una pista, el número que puede ser está cerca de ${
+    random + 7
+  } `
+  console.log(random)
+})
 
 checkIt.addEventListener('click', () => {
-  remember.innerHTML = rememberGuessNumbers(guessNumber);
-  alerts.textContent = checkRandom(random, guessNumber)[0];
+  remember.innerHTML = rememberGuessNumbers(guessNumber)
+  alerts.textContent = checkRandom(random, guessNumber)[0]
   if (checkRandom(random, guessNumber)[1] == 1) {
-    intentos.parentNode.textContent = 'Ala! a JUGAR DE NUEVO';
+    intentos.parentNode.textContent = '¡ALA! a JUGAR DE NUEVO'
     setTimeout(() => {
-      window.location.reload(true);
-    }, 3000);
+      window.location.reload(true)
+    }, 3000)
   }
-  const cuenta = counter();
-  if (cuenta > 0) intentos.textContent = cuenta;
+  guessNumber.value = ''
+  const cuenta = counter()
+  if (cuenta > 0) intentos.textContent = cuenta
   else {
-    intentos.parentNode.textContent = `
-    G
-         A
-             M
-                 E
-                                   O
-                                         V
-                                             E 
-                                                 R`;
-    setTimeout(() => window.location.reload(true), 1000);
+    intentos.parentNode.innerHTML = `<span class="bg-danger text-warning p-4">G A M E O V E R</span>`
+    setTimeout(() => window.location.reload(true), 1000)
   }
-});
+})
 
-cierra.addEventListener('click', () => window.location.reload());
+cierra.addEventListener('click', () => window.location.reload())

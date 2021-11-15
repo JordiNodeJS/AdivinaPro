3d3ac87ca57ea2fa57ab13ea4868da8a3cb4f2d8
JordiNodeJS <jordi.nodejs@gmail.com>
2021-11-15 03:57:10 +0100

    adivina por los pelos

--- js/adivinaDoom.js
+++ js/adivinaDoom.js
@@ -1,6 +1,34 @@
 const dom = {
   ranges: document.querySelectorAll('input[type="range"]'),
   badges: document.querySelectorAll('.badge'),
+  bodyModalranges: document.querySelectorAll('.modal-body span.badge'),
+}
+const NUM_MENOR = 'Tú número es menor que  el pensado.'
+const NUM_MAYOR = 'Tú número es mayor que  el pensado.'
+const EUREKA = 'EUREKA'
+const INTRO = 'Intruduce un número que es gratis.'
+
+export const getRandom = ([min, max]) => {
+  min = parseInt(min.value)
+  max = parseInt(max.value)
+  return Math.floor(Math.random() * (max + 1 - min)) + min
+  //   console.log(min, max, random)
+}
+
+export const checkRandom = (random, guessNumber) => {
+  random = parseInt(random)
+  guessNumber = parseInt(guessNumber.value)
+  return guessNumber < random
+    ? NUM_MENOR
+    : guessNumber > random
+    ? NUM_MAYOR
+    : guessNumber == guessNumber
+    ? EUREKA
+    : INTRO
+}
+let count = 5
+export function counter() {
+  return count > 0 ? --count : count
 }
 
 export default dom
--- js/main.js
+++ js/main.js
@@ -1,6 +1,30 @@
-import dom from './adivinaDoom.js'
+import dom, { getRandom, checkRandom, counter } from './adivinaDoom.js'
 
 for (let i = 0; i < dom.ranges.length; i++)
   dom.ranges[i].addEventListener('input', () => {
     dom.badges[i].textContent = dom.ranges[i].value
   })
+
+for (let i = 0; i < dom.bodyModalranges.length; i++) {
+  dom.bodyModalranges[i].textContent = dom.ranges[i].value
+}
+
+let random
+let count = 1
+console.log(++count)
+
+adivina.addEventListener('click', () => {
+  random = getRandom(dom.ranges)
+  console.log(random)
+})
+
+checkIt.addEventListener('click', () => {
+  alerts.textContent = checkRandom(random, guessNumber)
+  if (counter() > 0) intentos.textContent = counter()
+  else {
+    intentos.parentNode.textContent = 'G A M E   O V E R'
+    setTimeout(() => window.location.reload(true), 10000)
+  }
+})
+
+cierra.addEventListener('click', () => window.location.reload())
